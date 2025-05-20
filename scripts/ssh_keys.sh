450e4be083ad60d8b74c95c0addd0c76d0d79fdb
kirill-sch <schiroky.kirill@gmail.com>
2025-05-20 23:17:17 +0200

    Sleep and ls -l is for testing purposes. Other then that the file paths were wrong and they have been corrected. The SSH keys will be created inside the ssh_keys folder inside the users folder. The public key is shredded (deleted) after it has been copied to the correct folder.

--- scripts/ssh_keys.sh
+++ scripts/ssh_keys.sh
@@ -1,16 +1,22 @@
 # TODO: add passphase protection
+# TODO: path should be in a variable for shorter code
 generate_ssh_key () {
     username=$1
 
     echo "Generating ssh key for username: $username"
 
-    mkdir "../output/ssh_keys/$username"    
-    ssh-keygen -t rsa -b 2048 -f "../output/ssh_keys/$username"
+    mkdir "output/ssh_keys/$username"    
+    ssh-keygen -t rsa -b 2048 -f "output/ssh_keys/$username/$username" -q -N "" # TODO: add passphrase, without passphrase only suitable for testing environment
+    ls -l "output/ssh_keys/$username"
+
+    sleep 15s
 
     mkdir -p "/home/$username/.ssh"
     sudo chmod 700 "/home/$username/.ssh"
 
-    cp "../output/ssh_keys/$username.pub" "/home/$username/.ssh/authorized_keys"
+    cp "output/ssh_keys/$username/$username.pub" "/home/$username/.ssh/authorized_keys"
     sudo chmod 600 "/home/$username/.ssh/authorized_keys"
     sudo chown -R "$username":"$username" "/home/$username/.ssh/authorized_keys"
+
+    shred -u "output/ssh_keys/$username/$username.pub" 
 }
