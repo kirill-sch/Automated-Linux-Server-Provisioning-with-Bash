272aeed97d43d8d1e892402a026637cab282d739
kirill-sch <schiroky.kirill@gmail.com>
2025-05-31 15:19:19 +0200

    Add check to verify that the groups value is not empty.

--- scripts/user_setup.sh
+++ scripts/user_setup.sh
@@ -8,7 +8,7 @@ group_create () {
     for i in "${GROUP[@]}"; do
         output=$(getent group "$i")
 
-        if [[ -n $output ]]; then # if group is already created then skip creation
+        if [[ -n "$output" ]]; then 
             continue
         else 
             echo "Creating group: $i"
@@ -18,19 +18,20 @@ group_create () {
 }
 
 setup_user_and_groups () {      
-    username=$4  
-    group_list=$3
-    password="$(makepasswd --chars 10)"
-    # TODO: must check if the groups is empty then if it is add user to default group
+    username="$1"
+    groups="$2"
+    password="$(makepasswd --chars 10)"    
 
     echo "Generating user with username: $username"
 
-    group_create "$group_list"
+    if [[ -z "$groups" ]]; then
+        sudo useradd -m -s /bin/bash "$username"
+    else
+        group_create "$groups"
+        sudo useradd -m -G "$groups" -s /bin/bash "$username" # -m: creates home dir; -s: login shell
+    fi
 
-    sudo useradd -m -G "$group_list" -s /bin/bash "$username" # -m: creates home dir; -s: login shell
-    getent passwd "$username"
-
-    echo "$username:$password" | sudo chpasswd
+    echo "$username:$password" | sudo chpasswd # add password
     sudo passwd -n 7 "$username" # password can't be changed for given days
     sudo passwd -x 90 "$username" # password remains valid for given days
     sudo passwd -e "$username" # expire password, forced to change password at the next login
